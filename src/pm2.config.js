85e6d24fd6459c3294023ba060f86a38976517d2
Tolui <tobi418@gmail.com>
2021-11-06 09:19:54 +0800

    cluster

--- src/pm2.config.js
+++ src/pm2.config.js
@@ -1,7 +1,7 @@
 //@ts-ignore
 module.exports = {
     name: 'history',
-    script: 'dist/index.js',
+    script: 'index.js',
     max_memory_restart: '2000M',
     exec_mode: "cluster",
     instances: 'max',
